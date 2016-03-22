5585f9caf56068ecd208ac7a03f9feb9f7e06fa6
Laarni Astrid <laarni.astrid@gmail.com>
2016-03-22 11:57:39 -0600

    creating endpoints

---
+++ controls/userCtrl.js
@@ -0,0 +1,24 @@
+var User = require('../schemas/haunter.js');
+
+module.exports = {
+  create: function(req, res, next) {
+    var user = new User(req.body);
+    user.save(function(err, r) {
+      return err ? res.status(500).send(err) : res.status(200).send(r);
+    })
+  },
+  read: function(req, res, next) {
+    var query = {};
+    if (req.query.username && req.query.password) {
+      query = {
+        username: req.query.username,
+        password: req.query.password
+      }
+    } else {
+      query = {};
+    }
+    User.find(query, function(err, r) {
+      return err ? res.status(500).send(err) : res.status(200).send(r);
+    })
+  }
+}
