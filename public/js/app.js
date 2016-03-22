06525496746480f90fc041318bf12af4ba1ae38e
Laarni Astrid <laarni.astrid@gmail.com>
2016-03-22 17:03:05 -0600

    added base folders js and styles

---
+++ public/js/app.js
@@ -0,0 +1,33 @@
+angular.module('myApp', ['ui.router'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('login', {
+      url: '/',
+      templateUrl: './views/login.html',
+      controller: 'mainCtrl'
+    })
+    .state('signup', {
+      url: '/signup',
+      templateUrl: 'signup.html',
+      controller: 'mainCtrl'
+    })
+    .state('people', {
+      url: '/people',
+      templateUrl: './views/people.html',
+      controller: 'peopleCtrl'
+    })
+    .state('list', {
+      url: '/list',
+      templateUrl: './views/list.html',
+      controller: 'listCtrl'
+    })
+    .state('admin', {
+      url: '/admin',
+      templateUrl: '/views/admin.html',
+      controller: 'mainCtrl'
+    });
+
+    $urlRouterProvider.otherwise('/login');
+}); // end confif
