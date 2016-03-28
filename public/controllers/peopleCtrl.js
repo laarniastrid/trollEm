3d8c38cfed66dc2c5776eb20bcdb83789588fd38
Laarni Astrid <laarni.astrid@gmail.com>
2016-03-28 17:20:22 -0600

    working on styling

--- public/controllers/peopleCtrl.js
+++ public/controllers/peopleCtrl.js
@@ -13,27 +13,12 @@ angular.module('myApp')
   };
 
 
-
-
   $scope.addPerson = function() {
     console.log('clicked');
 
   };
 
 
-/*
-$scope.login = function(user) {
-  mainSvc.login(user).then(function(response) {
-  // console.log('testing');
-    if (response.data.userFound) {
-      console.log('user is found');
-      $location.path('people');
-    } else {
-      console.log('User not found');
-    }
-  });
-};
-*/
 
 $scope.showModal = false;
 $scope.modalToggle = function() {
