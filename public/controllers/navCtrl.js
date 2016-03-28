4cc834cd81db356cd55828b39348eb530a7424fe
Laarni Astrid <laarni.astrid@gmail.com>
2016-03-28 13:38:45 -0600

    updated styles

--- public/controllers/navCtrl.js
+++ public/controllers/navCtrl.js
@@ -14,23 +14,6 @@ angular.module('myApp')
   };
 
 
-});  // end navCtrl
+  
 
-/*
-<div class="modal" ng-if="showModal">
-  <div class="modal-overlay" ng-click="modalToggle()"></div>
-  <div class="modal-content">
-    <i class="fa fa-times" ng-click="modalToggle()"></i>
-    <div class="modal-content-wrapper">
-      <!-- <div>modal heading</div> -->
-      <!-- <div>modal stuff</div> -->
-      <h2>Send new message!<h2>
-      <form ng-submit="sendMessage(action) && modalToggle()" class="modal-form">
-        <textarea placeholder="message" ng-model="action.message" required></textarea>
-        <!-- <input type="date" value="new Data()" /> -->
-        <button type="submit">Send Message</button>
-      </form>
-    </div>
-  </div>
-</div>
-*/
+});  // end navCtrl
