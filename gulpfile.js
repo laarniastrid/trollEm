f617d0bcc0a523a137c04bae4dcac6a151936477
Astrid Sutton <astrid.sutton@wgu.edu>
2018-06-08 19:26:18 -0600

    updating gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,69 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    concat = require('gulp-concat'),
-    // uglify = require('gulp-uglify'),
-    uglifycss = require('gulp-uglifycss'),
-    ngAnnotate = require('gulp-ng-annotate'),
-    watcher = gulp.watch(['./main/**/*.js', './main/**/*.scss', './main/**/*.html'], ['default']);
+(() => {
+	let gulp = require('gulp');
+	let sass = require('gulp-sass');
+	let concat = require('gulp-concat');
+	let uglifyCss = require('gulp-uglifycss');
+	let ngAnnotate = require('gulp-ng-annotate');
+	let browserSync = require('browser-sync').create();
 
-watcher.on('change', function(event) {
-  console.log('File ' + event.path + ' was ' + event.type + ' at ' + new Date() + ' , running tasks...');
-});
+	let watchFiles = [
+		'./app/**/*.scss',
+		'./app/**/*.js',
+		'./app/**/*.html',
+	];
+	
+	let watcher = gulp.watch(watchFiles, ['default']);
 
-gulp.task('styles', function() {
-  return gulp.src('./main/**/*.scss')
-    .pipe(sass())
-    // .pipe(uglifycss())
-    .pipe(concat('styles.css'))
-    .pipe(gulp.dest('./public/styles'))
-});
+	gulp.task('default', [
+		'styles',
+		'js',
+		'html',
+	]);
 
-gulp.task('javascript', function() {
-  gulp.src(['./main/**/*.js', '!./main/jquery.js'])
-    .pipe(ngAnnotate())
-    // .pipe(uglify())
-    .pipe(concat('all.js'))
-    .pipe(gulp.dest('./public/scripts'))
-});
+	gulp.task('watch', ['browserSync', 'styles', 'js', 'html'], () => {
+		gulp.watch(watchFiles, ['default'])
+	});
 
-gulp.task('html', function() {
-  gulp.src('./main/**/*.html')
-    .pipe(gulp.dest('./public/html/'))
-});
+	watcher.on('change', (event) =>  {
+		console.log('file ' + 
+			event.path + 
+			' was ' + 
+			event.type + 
+			' at ' +
+			new Date() +
+			', running tasks...');
+	});
 
-gulp.task('default', ['styles', 'javascript', 'html']);
+	gulp.task('styles', () => {
+		return gulp.src('./app/**/*.scss')
+			.pipe(sass())
+			.pipe(concat('styles.css'))
+			.pipe(gulp.dest('./public/styles'))
+			.pipe(browserSync.reload({
+				stream: true
+			}))
+	});
+
+	gulp.task('js', () => {
+		return gulp.src('./app/**/*.js')
+			.pipe(ngAnnotate)
+			.pipe(concat('scripts.js'))
+			.pipe(gulp.dest('./public'))
+	});
+
+	gulp.task('html', () => {
+		gulp.src('./app/**/*.html')
+			.pipe(gulp.dest('./public/html'))
+			.pipe(browserSync.reload({
+				stream: true
+			}))
+	});
+
+	gulp.task('browserSync', () => {
+		browserSync.init({
+			server: {
+				baseDir: 'public'
+			}
+		})
+	})
+})();
